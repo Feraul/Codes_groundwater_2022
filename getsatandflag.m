645020d35898e0ce4ed40575e7f169644d1ab9bd
Fernando Contreras <ferlicapac@gmail.com>
2022-05-24 17:51:27 -0300

    comentarios

--- getsatandflag.m
+++ getsatandflag.m
@@ -5,7 +5,7 @@
 %TOPICOS ESPECIAIS EM DINAMICA DOS FLUIDOS COMPUTACIONAL
 %--------------------------------------------------------------------------
 %Subject: numerical routine to load the geometry in gmsh and to create...
-%the mesh parameter 
+%the mesh parameter
 %Type of file: FUNCTION
 %Criate date: 04/12/2013
 %Modify data:   /  /2013
@@ -14,7 +14,7 @@
 %Modified: Fernando Contreras, 2021
 %--------------------------------------------------------------------------
 %Goals:
-%    
+%
 
 %--------------------------------------------------------------------------
 %Additional comments: the "intertype" options are:
@@ -28,13 +28,13 @@
 function [satonvertices,satonedges,flagknownvert,flagknownedge] = ...
     getsatandflag(satinbound,injecelem,Sw,nflagc,nflagfacec,m)
 %Define global parameters:
-global coord bcflagc bedge inedge  visc numcase elemarea; 
+global coord bcflagc bedge inedge  visc numcase elemarea;
 
 %Initialize "bedgesize" and "inedgesize"
 bedgesize = size(bedge,1);
 inedgesize = size(inedge,1);
 coordsize = size(coord,1);
-if (numcase==249 || numcase==247)&& m==1 
+if (numcase==249 || numcase==247)&& m==1
     M = visc(2)/visc(1);
     R=log(M);
     % R<0 indicates that the contaminant is more viscous than the
@@ -47,16 +47,16 @@ else
     satonvertices = nflagc(:,2);
 end
 satonedges = zeros(bedgesize + inedgesize,1);
-%Initialize "knownvalinvert" and "knownvalinedge". It indicate if there is 
+%Initialize "knownvalinvert" and "knownvalinedge". It indicate if there is
 %or not a prescribed value.
 knownvalinvert =  nflagc(:,3);
 knownvalinedge = zeros(bedgesize,1);
 
-%Evaluate if there is non-null Neumann boundary condition (non-null Neumann 
-%flux, in pressure indicate saturation, by Dirichlet boundary condition 
+%Evaluate if there is non-null Neumann boundary condition (non-null Neumann
+%flux, in pressure indicate saturation, by Dirichlet boundary condition
 %prescribed).
 pointneumann = logical(bcflagc(:,1) > 200 & bcflagc(:,1) < 300 & ...
-        bcflagc(:,2) ~= 0);
+    bcflagc(:,2) ~= 0);
 
 %--------------------------
 %Saturation on the VERTICES
@@ -67,10 +67,10 @@ if any(pointneumann) && any(bcflagc(pointneumann,2) > 0) && ...
         (any(nflagfacec) || length(nflagfacec) > 1)
     %Find the flags for non-null Neumann boundary condition.
     nonullflag = bcflag(pointneumann,1);
-
+    
     %Swept all the edges on the boundary
     for i = 1:bedgesize
-        %The element belongs to vector "injecelem" and the flag of the 5th 
+        %The element belongs to vector "injecelem" and the flag of the 5th
         %column of "bedge" matches to "nonullflag"
         if (ismember(bedge(i,3),injecelem)) && (bedge(i,5) == nonullflag)
             %It points the position in "injecelem" corresponding the
@@ -86,56 +86,56 @@ if any(pointneumann) && any(bcflagc(pointneumann,2) > 0) && ...
             satonvertices(bedge(i,1:2)) = satinbound(pointposit);
             %It is an indication there is a known value over vertex
             knownvalinvert(bedge(i,1:2)) = 1;
-        end  %End of IF            
+        end  %End of IF
     end  %End of FOR
 end  %End of IF
 
 %Get the edges whose boudary condition were NOT attributed.
 iedge = 1:bedgesize;
-unknownedge = iedge(logical(knownvalinedge == 0));    
+unknownedge = iedge(logical(knownvalinedge == 0));
 %Get the vertices whose boudary condition were NOT attributed.
 ivert = 1:coordsize;
-unknownvert = ivert(logical(knownvalinvert == 0));    
+unknownvert = ivert(logical(knownvalinvert == 0));
 
 
-    %Calculate the saturation into each unknown vertex    
-    for i = 1:length(unknownvert)
-        inode = unknownvert(i);
-        %get the amount of elements surrounding the vertex evaluated.
-        [esurn,] = getsurnode(inode); 
-        %get the saturation into vertex evaluated.
-        satinvertex = getsatinvertex(elemarea(esurn),Sw(esurn));
-        %Attribute the value calculated to "satonvertices"
-        satonvertices(inode) = satinvertex;
-    end  %End of FOR (swept the vertices)    
+%Calculate the saturation into each unknown vertex
+for i = 1:length(unknownvert)
+    inode = unknownvert(i);
+    %get the amount of elements surrounding the vertex evaluated.
+    [esurn,] = getsurnode(inode);
+    %get the saturation into vertex evaluated.
+    satinvertex = getsatinvertex(elemarea(esurn),Sw(esurn));
+    %Attribute the value calculated to "satonvertices"
+    satonvertices(inode) = satinvertex;
+end  %End of FOR (swept the vertices)
 
 
 %-----------------------
 %Saturation on the EDGES
 
-%Calculate the saturation into each unknown edge (just in "bedge")    
+%Calculate the saturation into each unknown edge (just in "bedge")
 %The Saturation on the Midedge is obtained by Shared Volume Average.
 
-    %Swept "bedge"
-    for i = 1:length(unknownedge)
-        %Calculate the saturation into each unknown edge ("bedge")    
-        %satonedges(unknownedge(i)) = nflagfacec(i,2);
-         satonedges(unknownedge(i))= 0.5*(satonvertices(bedge(i,1)) + satonvertices(bedge(i,2)));
-        %It is an indication there is a known value over edge
-        if nflagfacec(i,1)<100
-            knownvalinedge(i)=1;
-        else
-            knownvalinedge(i)=0;
-        end
-    end  %End of FOR (swept the edges from "bedge")
+%Swept "bedge"
+for i = 1:length(unknownedge)
+    %Calculate the saturation into each unknown edge ("bedge")
+    %satonedges(unknownedge(i)) = nflagfacec(i,2);
+    satonedges(unknownedge(i))= 0.5*(satonvertices(bedge(i,1)) + satonvertices(bedge(i,2)));
+    %It is an indication there is a known value over edge
+    if nflagfacec(i,1)<100
+        knownvalinedge(i)=1;
+    else
+        knownvalinedge(i)=0;
+    end
+end  %End of FOR (swept the edges from "bedge")
 
-    %Swept "inedge"
-    for i = 1:inedgesize
-        %Calculate the saturation into each unknown edge ("inedge")    
-        satonedges(bedgesize + i) = ...
-            0.5*(satonvertices(inedge(i,1)) + satonvertices(inedge(i,2)));
-        
-    end  %End of FOR (swept the edges from "inedge")    
+%Swept "inedge"
+for i = 1:inedgesize
+    %Calculate the saturation into each unknown edge ("inedge")
+    satonedges(bedgesize + i) = ...
+        0.5*(satonvertices(inedge(i,1)) + satonvertices(inedge(i,2)));
+    
+end  %End of FOR (swept the edges from "inedge")
 
 
 %Fill the flag vectors:
